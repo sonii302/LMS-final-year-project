3c1b7fd7737992fb62d787251745c41f9169399d
Sonii302 <soniprasad302@gmail.com>
2025-06-11 01:04:09 +0530

    first commit

---
+++ client/src/components/student/SearchBar.jsx
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import { assets } from '../../assets/assets'
+import { data, useNavigate } from 'react-router-dom'
+
+const SearchBar = () => {
+
+  const navigate = useNavigate()
+  const [input, setInput] = useState('')
+
+  const onSearchHandeler = (e)=>{
+    e.preventDefault()
+    navigate('/course-list/' + input)
+  }
+  return (
+      <form onSubmit={onSearchHandeler} className='max-w-xl w-full md:b-14 h-12 flex items-center bg-white border border-gray-500/20 rounded'>
+        <img src={assets.search_icon} alt="search_icon" className='md:w-auto w-10
+        px-3' />
+        <input onChange={e => setInput(e.target.value)} value={input}
+         type="text" placeholder='Search For courses' className='w-full h-full outline-none text-gray-500/80' />
+        <button type='submit' className='bg-blue-600 rounded text-white md:px-10 px-7 md:py-3 py-2 mx-1'>Search</button>
+      </form>
+  )
+}
+
+export default SearchBar
